e3241cbf2ef57bec957e8aad7c1d5d33a2f6d582
Shiggy <65451703+Core-commits@users.noreply.github.com>
2020-06-26 21:28:14 +0200

    Create SimpleBot.js

---
+++ General Game Scripts/SimpleBot.js
@@ -0,0 +1,7 @@
+// script made by Rares :)
+let bot = new Bot("Name")
+  bot.setAvatar() // set the avatar of the bot ( this is only working with real player's outfits )
+  Game.newBot(bot)
+  bot.setSpeech("Text") // the thing that you want the bot to say
+  bot.setPosition(new Vector3(-13,16,0)) // the position
+  bot.setRotation(new Vector3(0,0,90)) // the rotation
