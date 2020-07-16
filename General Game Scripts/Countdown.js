cf126abb4ded0e44af4042869a6da0214a3128e7
Shiggy <65451703+Core-commits@users.noreply.github.com>
2020-07-16 13:30:31 +0200

    Update Countdown.js

--- General Game Scripts/Countdown.js
+++ General Game Scripts/Countdown.js
@@ -7,8 +7,8 @@ Game.on("playerJoin", (player) => {
             console.log(VAR);
             console.log(i);
             await sleep(1000)
-        
-        if (i == 9){
+        i2 = i+1
+        if (i2 == VAR){
             player.centerPrint("Game has started!!!", 2)
         }
     }
