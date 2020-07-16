d744a0edee7884c8b9f7c47a46df401eb8c8ef5f
Shiggy <65451703+Core-commits@users.noreply.github.com>
2020-07-16 13:30:57 +0200

    Update Countdown.js

--- General Game Scripts/Countdown.js
+++ General Game Scripts/Countdown.js
@@ -1,13 +1,12 @@
 Game.on("playerJoin", (player) => {
     player.on("initialSpawn", async() => {
         VAR = 10;
-        i2= 0;
         for (let i = 0; i < VAR; i++) {
             player.topPrint(`\c4Game Starts in  ${VAR - i} seconds.`);
             console.log(VAR);
             console.log(i);
             await sleep(1000)
-        i2 = i+1
+        i2 = i+1;
         if (i2 == VAR){
             player.centerPrint("Game has started!!!", 2)
         }
