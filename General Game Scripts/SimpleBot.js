2d6493366fc5bd4bfc7cf350809293bfe19cf235
Shiggy <65451703+Core-commits@users.noreply.github.com>
2020-07-17 12:04:21 +0200

    Fix "NewBot"

--- General Game Scripts/SimpleBot.js
+++ General Game Scripts/SimpleBot.js
@@ -1,7 +1,31 @@
-// script made by Rares :)
-let bot = new Bot("Name")
-  bot.setAvatar() // set the avatar of the bot ( this is only working with real player's outfits )
-  Game.newBot(bot)
-  bot.setSpeech("Text") // the thing that you want the bot to say
-  bot.setPosition(new Vector3(-13,16,0)) // the position
-  bot.setRotation(new Vector3(0,0,90)) // the rotation
+Game.on("initialSpawn", (player) => {
+
+   
+   
+   let zombie = new Bot("Zombie")
+
+    let outfit = new Outfit(zombie)
+    .body("#0d9436")
+    .torso("#694813")
+    .rightLeg("#694813")
+    .leftLeg("#694813")
+
+    Game.newBot(zombie)
+
+    // We use bot.setinterval so that when the zombie is destroyed, the loop clears.
+    // It's good practice to do this to avoid memory leaks.
+    zombie.setInterval(() => {
+    let target = zombie.findClosestPlayer(20)
+
+    if (!target) return zombie.setSpeech("")
+
+    zombie.setSpeech("BRAAINNNSSS!")
+
+    zombie.moveTowardsPlayer(target, 8)
+    }, 10)
+
+    let touchEvent = zombie.touching((p) => {
+    Game.messageAll(`[#ff0000]${p.username} was eaten by a zombie!`)
+    p.kill()
+    })
+})
