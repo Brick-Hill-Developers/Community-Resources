07c0ac7908295c9b522204671c254a1fcfa63e13
Shiggy <65451703+Core-commits@users.noreply.github.com>
2020-08-27 13:00:39 +0200

    Added Vanish script

---
+++ General Game Scripts/Administration Tools/vanish (1).js
@@ -0,0 +1,21 @@
+Game.command("vanish", (player) => {
+    // Make the player disappear
+    player._removePlayer()
+    // Say goodbye to the player
+    Game.messageAll(`\\c6[SERVER]: \\c0${player.username} has left the server!`)
+})
+Game.command("novanish", (player) => {
+    // Make the player re-appear
+    new PacketBuilder(3)
+        .write("uint8", 1)
+        .write("uint32", player.netId)
+        .write("string", player.username)
+        .write("uint32", player.userId)
+        .write("uint8", player.admin)
+        .write("uint8", player.membershipType)
+        .broadcastExcept([player]);
+    // Greet the player
+    player._greet()
+    // Load the outfit
+    player.setAvatar(player.userId)
+})
