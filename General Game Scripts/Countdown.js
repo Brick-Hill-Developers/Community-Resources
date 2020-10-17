4822f7e93e8c5c633915341321ccc7365805ebb0
Shiggy <65451703+Core-commits@users.noreply.github.com>
2020-10-17 12:06:41 +0200

    Fix countdown

--- General Game Scripts/Countdown.js
+++ General Game Scripts/Countdown.js
@@ -1,15 +1,15 @@
 Game.on("playerJoin", (player) => {
-    player.on("initialSpawn", async() => {
-        VAR = 10;
+    player.on("initialSpawn", async () => {
+        let VAR = 10
+        
         for (let i = 0; i < VAR; i++) {
-            player.topPrint(`\c4Game Starts in  ${VAR - i} seconds.`);
-            console.log(VAR);
-            console.log(i);
+            player.topPrint(`Game starts in ${VAR - i} seconds.`)
+            console.log(VAR)
+            console.log(i)
             await sleep(1000)
-        i2 = i+1;
-        if (i2 == VAR){
-            player.centerPrint("Game has started!!!", 2)
+            if (i == 9) {
+                player.centerPrint("Game has started", 2)
+            }
         }
-    }
     })
- })
+})
