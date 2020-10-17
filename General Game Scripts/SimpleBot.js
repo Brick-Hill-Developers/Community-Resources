8b8a53ec09bfa0a5e73a554b63456a04dc1a393e
Shiggy <65451703+Core-commits@users.noreply.github.com>
2020-10-17 12:05:17 +0200

    Document and remove bad code

--- General Game Scripts/SimpleBot.js
+++ General Game Scripts/SimpleBot.js
@@ -1,31 +1,27 @@
-Game.on("initialSpawn", (player) => {
+let zombie = new Bot("Zombie")
 
-   
-   
-   let zombie = new Bot("Zombie")
-
-    let outfit = new Outfit(zombie)
+let outfit = new Outfit(zombie) // zombie outfit
     .body("#0d9436")
     .torso("#694813")
     .rightLeg("#694813")
     .leftLeg("#694813")
 
-    Game.newBot(zombie)
+Game.newBot(zombie) // crete the bot instance 
 
-    // We use bot.setinterval so that when the zombie is destroyed, the loop clears.
-    // It's good practice to do this to avoid memory leaks.
-    zombie.setInterval(() => {
+// We use bot.setinterval so that when the zombie is destroyed, the loop clears.
+// It's good practice to do this to avoid memory leaks.
+zombie.setInterval(() => {
     let target = zombie.findClosestPlayer(20)
 
-    if (!target) return zombie.setSpeech("")
+    if (!target) return zombie.setSpeech("") // if there isnt a player nearby do 
 
-    zombie.setSpeech("BRAAINNNSSS!")
+    zombie.setSpeech("BRAAINNNSSS!")  // if there is a player nearby do 
 
-    zombie.moveTowardsPlayer(target, 8)
-    }, 10)
+    zombie.moveTowardsPlayer(target, 8) // move to the player
+}, 10)
 
-    let touchEvent = zombie.touching((p) => {
+let touchEvent = zombie.touching((p) => { // Bot touches Player
     Game.messageAll(`[#ff0000]${p.username} was eaten by a zombie!`)
-    p.kill()
-    })
+    // p.kill() to kill a player intantly 
+   // p.setHealth(p.health-5) to reduce the player health by 5
 })
